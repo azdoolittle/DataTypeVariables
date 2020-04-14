2bd83159d47913d3bb0b63b6f5b7370e9af1f3fe
azdoolittle <drewdoolittle@gmail.com>
2020-04-14 10:07:24 -0400

    Java data type variables exercise

---
+++ src/DataTypeVariables.java
@@ -0,0 +1,23 @@
+public class DataTypeVariables {
+    public static void main(String[] args)
+    {
+
+        boolean isABoolean = true;
+        char singleChar = 'A';
+        byte byteInteger = 127;
+        short shortInteger = 32767;
+        int intInteger = 2146483647;
+        long longInteger = 15000000000L;
+        float floatNumber = 6.85f;
+        double doubleFloatNumber = 19.69;
+
+        System.out.println("isABoolean: " + isABoolean);
+        System.out.println("singleChar: " + singleChar);
+        System.out.println("byteInteger: " + byteInteger);
+        System.out.println("shortInteger: " + shortInteger);
+        System.out.println("intInteger: " + intInteger);
+        System.out.println("longInteger: " + longInteger);
+        System.out.println("floatNumber: " + floatNumber);
+        System.out.println("doubleFloatNumber: " + doubleFloatNumber);
+    }
+}
